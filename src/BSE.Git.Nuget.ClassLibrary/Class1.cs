3e61672bd43843d9ca3caa5208649d65a276ef34
uwe-e <git.bsetunes@outlook.com>
2022-07-22 12:33:00 +0200

    initial commit

---
+++ src/BSE.Git.Nuget.ClassLibrary/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace BSE.Git.Nuget.ClassLibrary
+{
+    public class Class1
+    {
+        public string GetHello(string text)
+        {
+            return $"{ text} {DateTime.Now}";
+        }
+    }
+}
